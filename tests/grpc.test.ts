70b38d4d454ad6f4963b866701760c3b861cc2f9
casuallyhostile <andrew.abramov00@gmail.com>
2024-02-29 15:13:17 +0100

    repro of the cached variables

---
+++ tests/grpc.test.ts
@@ -0,0 +1,65 @@
+import 'json-bigint-patch';
+import { join } from 'path';
+import { readFile } from 'fs-extra';
+import { findAndParseConfig } from '@graphql-mesh/cli';
+import { getMesh, MeshInstance } from '@graphql-mesh/runtime';
+import { printSchemaWithDirectives } from '@graphql-tools/utils';
+import { Server } from '@grpc/grpc-js';
+import { startServer as startGrpcServer } from '../start-server';
+
+describe('gRPC Example', () => {
+  let mesh: MeshInstance;
+  let grpc: Server;
+  beforeAll(async () => {
+    const config = await findAndParseConfig({
+      dir: join(__dirname, '..'),
+    });
+    mesh = await getMesh(config);
+    grpc = await startGrpcServer(300);
+  });
+  it('should generate correct schema', async () => {
+    expect(printSchemaWithDirectives(mesh.schema)).toMatchSnapshot('grpc-schema');
+  });
+  it('should get movies correctly', async () => {
+    const GetMoviesQuery = await readFile(
+      join(__dirname, '../example-queries/GetMovies.query.graphql'),
+      'utf8',
+    );
+    const result = await mesh.execute(GetMoviesQuery, undefined);
+    expect(result).toMatchSnapshot('get-movies-grpc-example-result');
+  });
+  it('should fetch movies by cast as a stream correctly', async () => {
+    const MoviesByCastStream = await readFile(
+      join(__dirname, '../example-queries/MoviesByCast.stream.graphql'),
+      'utf8',
+    );
+    const result = await mesh.execute(MoviesByCastStream, undefined);
+    let i = 0;
+    for await (const item of result as AsyncIterable<any>) {
+      expect(item).toMatchSnapshot(`movies-by-cast-grpc-example-result-stream-${i++}`);
+    }
+  });
+  it('should fetch movies by cast as a subscription correctly', async () => {
+    const MoviesByCastSubscription = await readFile(
+      join(__dirname, '../example-queries/MoviesByCast.subscription.graphql'),
+      'utf8',
+    );
+    const result = await mesh.execute(MoviesByCastSubscription, undefined);
+    let i = 0;
+    for await (const item of result as AsyncIterable<any>) {
+      expect(item).toMatchSnapshot(`movies-by-cast-grpc-example-result-subscription-${i++}`);
+    }
+  });
+  afterAll(async () => {
+    mesh?.destroy();
+    await new Promise<void>((resolve, reject) => {
+      grpc?.tryShutdown(err => {
+        if (err) {
+          reject(err);
+        } else {
+          setTimeout(resolve, 1000);
+        }
+      });
+    });
+  });
+});
